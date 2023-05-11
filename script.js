591acb8e651c6d9d48ff134f3e669d68de9b3823
Norman <normant_padovani@hotmail.com>
2023-05-11 17:36:15 -0400

    I have managed to display a sprite image when a collision is detected but not the animation

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ ctx.arc(100, 100, 50, 0, Math.PI * 2, false);
 ctx.fillStyle = "white";
 ctx.fill();
 
+let collision = false;
+
 let missile = {
     x: canvas.width / 2,
     y: canvas.height,
-    speed: 2,
+    speed: 4,
     dx: 0,
     dy: 0,
     isConnected: true
@@ -28,27 +30,52 @@ let target = {
     y: Math.random() * canvas.height
 };
 
-// Load the image
+// Here I Load the images
 let missileImage = new Image();
+let explosionImage = new Image();
+
+// Count how many images have been loaded
+let imagesLoaded = 0;
+
+// Set up the onload function for both images
+missileImage.onload = explosionImage.onload = function() {
+    imagesLoaded++;
+
+    // If both images have been loaded, start the simulation
+    if (imagesLoaded === 2) {
+        // Call updateMissilePosition every 20ms
+        setInterval(updateMissilePosition, 20);
+    }
+};
+
+// Set the src after setting up the onload function
 missileImage.src = "missile.png";
+explosionImage.src = "explosion.png";
+
+
 
 // Update drawObjects function
 function drawObjects() {
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-   // Draw the missile
-   ctx.save();  // Save the current state of the canvas
-   ctx.translate(missile.x, missile.y);  // Move the origin to the missile's location
-   ctx.rotate(Math.atan2(missile.dy, missile.dx) + Math.PI/2);  // Rotate the canvas by the angle of movement
-   ctx.drawImage(missileImage, -missileImage.width / 2, -missileImage.height / 2);  // Draw the missile centered at the new origin
-   ctx.restore();  // Restore the canvas state to how it was before we moved the origin
+    if (collision) {
+        // Draw the explosion at the target's location
+        ctx.drawImage(explosionImage, target.x - 50, target.y - 50, 100, 100);
+    } else {
+        // Draw the missile
+        ctx.save();  // Save the current state of the canvas
+        ctx.translate(missile.x, missile.y);  // Move the origin to the missile's location
+        ctx.rotate(Math.atan2(missile.dy, missile.dx) + Math.PI/2);  // Rotate the canvas by the angle of movement
+        ctx.drawImage(missileImage, -missileImage.width / 2, -missileImage.height / 2);  // Draw the missile centered at the new origin
+        ctx.restore();  // Restore the canvas state to how it was before we moved the origin
 
-    // Draw the target
-    ctx.beginPath();
-    ctx.arc(target.x, target.y, 20, 0, Math.PI * 2, false);
-    ctx.fillStyle = "red";
-    ctx.fill();
+        // Draw the target
+        ctx.beginPath();
+        ctx.arc(target.x, target.y, 20, 0, Math.PI * 2, false);
+        ctx.fillStyle = "red";
+        ctx.fill();
+    }
 }
 
 // Initial drawing of objects
@@ -56,20 +83,28 @@ drawObjects();
 
 
 function updateMissilePosition() {
-    // Calculate direction vector from missile to target
-    let dx = target.x - missile.x;
-    let dy = target.y - missile.y;
+    // Only update position if there has been no collision
+    if (!collision) {
+        // Calculate direction vector from missile to target
+        let dx = target.x - missile.x;
+        let dy = target.y - missile.y;
 
-    // Calculate distance between missile and target
-    let distance = Math.sqrt(dx * dx + dy * dy);
+        // Calculate distance between missile and target
+        let distance = Math.sqrt(dx * dx + dy * dy);
 
-    // Normalize direction vector and multiply by speed
-    missile.dx = (dx / distance) * missile.speed;
-    missile.dy = (dy / distance) * missile.speed;
+        // If distance is less than the sum of the radii, a collision has occurred
+        if (distance < 20 + 10) {
+            collision = true;
+        } else {
+            // Normalize direction vector and multiply by speed
+            missile.dx = (dx / distance) * missile.speed;
+            missile.dy = (dy / distance) * missile.speed;
 
-    // Add dx and dy to the missile's x and y coordinates
-    missile.x += missile.dx;
-    missile.y += missile.dy;
+            // Add dx and dy to the missile's x and y coordinates
+            missile.x += missile.dx;
+            missile.y += missile.dy;
+        }
+    }
 
     // Redraw the objects with their new positions
     drawObjects();
@@ -78,3 +113,30 @@ function updateMissilePosition() {
 // Call updateMissilePosition every 20ms
 setInterval(updateMissilePosition, 20);
 
+function animateExplosion() {
+    let explosion = document.getElementById('explosion');
+    explosion.style.left = `${target.x}px`;
+    explosion.style.top = `${target.y}px`;
+
+    let frame = 0;
+    let numFrames = 7;  // The total number of frames in your sprite sheet
+    let frameWidth = 240;  // The width of each frame in your sprite sheet
+    let frameRate = 500;  // The time (in milliseconds) each frame should be shown
+
+    // This interval will run every 'frameRate' milliseconds
+    let interval = setInterval(() => {
+        frame++;
+
+        // If we've shown all the frames, reset everything
+        if (frame >= numFrames) {
+            clearInterval(interval);
+            explosion.style.backgroundPosition = '0px 0px';
+            explosion.style.display = 'none';
+        } else {
+            // Shift the background image to show the next frame
+            // Note: Make sure the sprites in your sprite sheet are arranged horizontally
+            explosion.style.backgroundPosition = `${-frame * frameWidth}px 0px`;
+            explosion.style.display = 'block';
+        }
+    }, frameRate);
+}
